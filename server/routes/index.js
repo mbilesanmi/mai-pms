48c1806bdba7854988827a193f45bd501c15a042
Maranatha A. Ilesanmi <maranatha.ilesanmi@gmail.com>
2018-10-12 22:14:32 +0100

    Add create location route

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+const locationController = require('../controllers/location');
+
+module.exports = (app) => {
+  app.route('/locations')
+    .post(locationController.create)
+  
+  app.get('/', (req, res) => res.status(200).send({
+    message: "Welcome to Mai PMS.",
+  }));
+
+  app.get('/*', (req, res) => res.status(404).send({
+    message: "Page not found",
+  }));
+}

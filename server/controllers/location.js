ec52811c9d80652887e463dcb9aa8973200adce6
Maranatha A. Ilesanmi <maranatha.ilesanmi@gmail.com>
2018-10-12 22:00:33 +0100

    Add create location endpoint

---
+++ server/controllers/location.js
@@ -0,0 +1,44 @@
+const Location = require('../models').Location;
+
+
+module.exports = {
+  create(req, res) {
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).send({
+        message: 'Request cannot be empty'
+      });
+    };
+    const { name, parentLocation, zip, male, female } = req.body;
+
+    const createLocation = (parentLoc = null) => Location.create({
+      name,
+      parentLocation: parentLoc,
+      zip,
+      male,
+      female
+    })
+    .then(location => {
+      if (location) {
+        return res.status(201).send({
+          location,
+          message: 'Location saved successfully'
+        });
+      }
+    })
+    .catch(e => res.status(404).send({ message: e.errors || e }));
+
+    if (parentLocation) {
+      Location.findOne({ where: { id: parentLocation } })
+      .then((loc) => {
+        if (!loc) {
+          return res.status(400).send({ message: 'Parent Location not found' });
+        } else {
+          createLocation(loc.id);
+        }
+      }) 
+      .catch(e => res.status(400).send({ message: e.errors || e  }));
+    } else {
+      createLocation();
+    }
+  }
+}

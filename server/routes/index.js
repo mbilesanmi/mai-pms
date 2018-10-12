21eb281e594c6caa043436ae60541bdafb98beca
Maranatha A. Ilesanmi <maranatha.ilesanmi@gmail.com>
2018-10-12 23:00:53 +0100

    Add get one location endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,9 @@ const locationController = require('../controllers/location');
 module.exports = (app) => {
   app.route('/locations')
     .post(locationController.create)
+
+  app.route('/locations/:locationId')
+    .get(locationController.getOneLocation)
   
   app.get('/', (req, res) => res.status(200).send({
     message: "Welcome to Mai PMS.",

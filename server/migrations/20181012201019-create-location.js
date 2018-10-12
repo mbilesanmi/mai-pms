b196024966293b18b05303098eed960bb51b0393
Maranatha A. Ilesanmi <maranatha.ilesanmi@gmail.com>
2018-10-12 21:15:31 +0100

    Add Location model

---
+++ server/migrations/20181012201019-create-location.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Locations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      parentLocation: {
+        type: Sequelize.INTEGER
+      },
+      zip: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        unique: true
+      },
+      male: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      female: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Locations');
+  }
+};
